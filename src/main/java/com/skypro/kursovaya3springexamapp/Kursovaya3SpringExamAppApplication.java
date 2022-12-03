04d270f1163c3031e9f55465ab53205e7f8cf221
ruslaan.gilazefwgm <ruslaan.gilazefwgm@gmail.com>
2022-12-03 08:35:05 +0300

    empty

---
+++ src/main/java/com/skypro/kursovaya3springexamapp/Kursovaya3SpringExamAppApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.kursovaya3springexamapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kursovaya3SpringExamAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kursovaya3SpringExamAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/kursovaya3springexamapp/Kursovaya3SpringExamAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.kursovaya3springexamapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kursovaya3SpringExamAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
